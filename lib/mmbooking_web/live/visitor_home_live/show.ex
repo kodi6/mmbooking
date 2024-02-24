d88496bcc17ba5e0321618c3b6d5893f3b8f744b
kodi6 <punniyakodi@auroville.org.in>
2024-02-24 15:44:00 +0530

    completed select name

--- lib/mmbooking_web/live/visitor_home_live/show.ex
+++ lib/mmbooking_web/live/visitor_home_live/show.ex
@@ -6,7 +6,23 @@ defmodule MmbookingWeb.VisitorHomeLive.Show do
   def mount(params, _session, socket) do
     id = Map.get(params, "id")
     visitor = Visitors.get_visitor_by_id(id)
+    visitors = Visitors.get_visitors_by_email(visitor.email)
     {:ok,
+    socket
+    |> assign(:visitor, visitor)
+    |> assign(:visitors, visitors)
+  }
+  end
+
+
+
+
+  def handle_event("change_name", %{"first_name" => first_name}, socket) do
+    visitor = List.first(Enum.filter(socket.assigns.visitors, fn visitor -> visitor.first_name == first_name end))
+
+
+
+    {:noreply,
     socket
     |> assign(:visitor, visitor)
   }
