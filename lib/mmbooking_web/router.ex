ee26efb1b8d9dfeaaf5e62195a7af6cd0ef8128a
kodi6 <punniyakodi@auroville.org.in>
2024-03-05 12:10:10 +0530

    authentication&roll_layouts

--- lib/mmbooking_web/router.ex
+++ lib/mmbooking_web/router.ex
@@ -1,21 +1,34 @@
 defmodule MmbookingWeb.Router do
   use MmbookingWeb, :router
 
+  import MmbookingWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
-    plug :put_root_layout, html: {MmbookingWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
+
+
+  pipeline :liveview do
+    plug :put_root_layout, html: {MmbookingWeb.Layouts, :root}
+  end
+
+  pipeline :admin do
+
+    plug :put_root_layout, html: {MmbookingWeb.Layouts, :admin_layout}
+  end
+
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/", MmbookingWeb do
-    pipe_through :browser
+    pipe_through [:browser, :liveview]
 
     get "/", PageController, :home
     live "/mmaccess", WelcomeLive.Index, :index
@@ -30,8 +43,16 @@ defmodule MmbookingWeb.Router do
     live "/mmaccess/visitor_page/personal_details/:id/bookings", BookingLive.Index, :index
     live "/mmaccess/visitor_page/personal_details/:id/bookings/new_booking", BookingLive.Index, :edit
     live "/try", TryLive.Index, :index
+  end
 
 
+  scope "/admin", MmbookingWeb do
+    pipe_through [:browser, :admin]
+
+    scope "/" do
+      pipe_through [:require_authenticated_user]
+      live "/", AdminLive.Index, :index
+    end
   end
 
   # Other scopes may use custom stacks.
@@ -53,6 +74,44 @@ defmodule MmbookingWeb.Router do
 
       live_dashboard "/dashboard", metrics: MmbookingWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", MmbookingWeb do
+    pipe_through [:browser, :admin, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{MmbookingWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", MmbookingWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{MmbookingWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", MmbookingWeb do
+    pipe_through [:browser, :admin]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{MmbookingWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
