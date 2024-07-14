aaccb75d5e0695e18f292a7f3b3f48d25a985a5e
kodi6 <punniyakodi@auroville.org.in>
2024-07-14 23:44:36 +0530

    rework in progress

--- lib/mmbooking_web/live/session_live/index.ex
+++ lib/mmbooking_web/live/session_live/index.ex
@@ -15,9 +15,7 @@ defmodule MmbookingWeb.SessionLive.Index do
     |> assign(:sessions, [])}
   end
 
-  def handle_params(params, _url, socket) do
-    if Map.has_key?(params, "template_id") do
-      template_id = Map.get(params, "template_id")
+  def handle_params(%{"template_id" => template_id}, _url, socket) do
            template = Templates.get_template(template_id)
            templates =  Templates.list_templates()
            sessions = Sessions.get_sessions_tmp_id(template_id)
@@ -25,10 +23,13 @@ defmodule MmbookingWeb.SessionLive.Index do
           socket
           |> assign(:templates, templates)
           |> assign(:template_name, template.name)
-          |> assign(:sessions, sessions)}
-    else
-      {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-    end
+          |> assign(:sessions, sessions)
+          |> redirect(to: ~p"/sessions/?template_id=#{template.id}")}
+
+  end
+
+  def handle_params(params, _url, socket) do
+   {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
   defp apply_action(socket, :edit_session, %{"session_id" => session_id}) do
@@ -54,9 +55,18 @@ defmodule MmbookingWeb.SessionLive.Index do
     socket
   end
 
-  def handle_event("template", params, socket) do
-    name = Map.get(params, "name")
+  def handle_event("template", %{"name" => name} = params, socket) do
     template = Templates.get_template_by_name(name)
+
+
+
+
+
+
+
+
+
+
     if template do
       {:noreply,
         socket
@@ -78,6 +88,18 @@ defmodule MmbookingWeb.SessionLive.Index do
       |> assign(:template_name, template.name)
       |> assign(:sessions, sessions)}
     end
+  end
+
+  defp create_template(name) do
+    template = Templates.create_template(%{"name" => name})
+
+    sessions = if String.contains?(name, "darshan") do
+      Sessions.create_darshan_session(template.id)
+    else
+      Sessions.create_default_session(template.id)
+    end
+
+    {template, sessions}
   end
 
   def handle_event("change_template", %{"name" => name}, socket) do
